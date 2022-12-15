03dd52e86934aac18e10d7cefb0f9f14ab1961c1
Rogerio Pereira <hi@rogeriopereira.dev>
2022-12-14 23:12:47 -0500

    AULA: 04 - CUIDADOS COM MEMÓRIA EM APLICAÇÕES LARAVEL OCTANE

---
+++ app/Services/ExampleService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+
+class ExampleService
+{
+    public static $values;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\ExampleService;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('test', function(){
+    ExampleService::$values[] = rand(4,2);
+
+    dd(ExampleService::$values);
 });
